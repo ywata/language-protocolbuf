c9a318468921dc9d54166211b87275f2d7e36911
Yasu Watanabe <ywataywatay@gmail.com>
2021-04-11 19:26:11 +0900

    Add test tool in Main.hs.

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -5,13 +6,21 @@ module Main where
 import Control.Monad (join)
 import System.Exit
 import Data.Maybe
+import Data.Binary.Get
 import qualified Data.Text as T hiding(map, concatMap)
 import qualified Options.Declarative as D
 import Text.Megaparsec.Error
 
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BL
+
+import Control.Applicative
 import Control.Monad.IO.Class
+import Control.Exception (bracket)
+import System.IO
 
 import Language.ProtocolBuffers
+import Language.ProtocolBuffers.Wire
 
 instance ShowErrorComponent Char where
   showErrorComponent c = show c
@@ -29,9 +38,37 @@ parse file = do
       liftIO $ putStrLn $ errorBundlePretty bndl
       liftIO $ exitWith (ExitFailure 1)
 
+runAsDummyPlugin :: D.Arg "file to be saved" FilePath -> D.Cmd "Dump plugin input" ()
+runAsDummyPlugin file = do
+  let dumpFile = D.get file
+  wfs <- liftIO $ copyStdinToFile dumpFile
+  return ()
+
+scan :: D.Arg "file to be scan" FilePath -> D.Cmd "Scan file and show summary" ()
+scan file = do
+  let file' = D.get file
+  wfs <- liftIO $ scanFile file'
+  liftIO $ print wfs
+  return ()
+
+copyStdinToFile :: FilePath -> IO([WireField a])
+copyStdinToFile path = bracket (openFile path WriteMode) hClose
+  $ \h -> do
+            bs <- BL.getContents
+            let !wfs = runGet (many getWireField) bs
+            return wfs
+
+scanFile :: FilePath -> IO([WireField a])
+scanFile path = bracket (openFile path ReadMode) hClose
+  $ \h -> do
+            bs <- BL.hGetContents h
+            let !wfs = runGet (many getWireField) bs
+            return wfs
+
 main :: IO()
 main =   D.run_ $ D.Group "Group of actions"
   [
     D.subCmd "parse" parse
+  , D.subCmd "plugin"  runAsDummyPlugin
+  , D.subCmd "scan"    scan
   ]
-
--- bin/dump.sh
+++ bin/dump.sh
@@ -3,7 +3,7 @@
 parser=`find dist-newstyle -name parse-proto -type f`
 if [ -x ${parser} ]; then 
   # do nothing
-  ${parser} dump
+  ${parser} plugin out.dat
 else
   echo "no parser found"
   exit 1
